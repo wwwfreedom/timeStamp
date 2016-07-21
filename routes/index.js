431bea2347107815df765c71bcf232c006401f9a
Kevin <tig3r86@gmail.com>
2016-07-21 22:43:50 +0930

    fix bug with natural language date parseing

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/*', function(req, res, next) {
   // set the valid date format
-  let dateFormats = ['MM-DD-YYYY', 'DD-MM-YYYY', 'MMMM-DD-YYYY', 'x', 'X']
+  let dateFormats = ['MM-DD-YYYY', 'DD-MM-YYYY', 'MMMM-DD-YYYY', 'MMMM-D-YYYY', 'x', 'X']
   // parse userInput into a moment date object in accordance to date formats
-  let date = moment(req.params[0].substring(1), dateFormats)
+  console.log(req.params[0])
+  let date = moment(req.params[0], dateFormats)
+  // console.log(date)
   // check if date is valid using isValid moment method
   if (date.isValid()) {
     // response with the unix and natural time format
